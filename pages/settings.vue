6930c4d88293f950d285ccaee87d3cd9fc2f1cee
savefimvasil <savefimvasil@gmail.com>
2020-10-25 17:26:11 +0200

    fix

--- pages/settings.vue
+++ pages/settings.vue
@@ -17,6 +17,9 @@
       <p v-if="successMessage">
         {{ successMessage }}
       </p>
+      <p v-if="errorMessage">
+        {{ errorMessage }}
+      </p>
     </div>
   </section>
 </template>
@@ -30,9 +33,10 @@
     data () {
       return {
         params: {
-          timeout: 1
+          timeout: 5
         },
-        successMessage: ''
+        successMessage: '',
+        errorMessage: ''
       }
     },
     async mounted () {
@@ -57,6 +61,13 @@
       },
 
       async updateSettings () {
+        if (this.params.timeout < 5 || this.params.timeout > 99) {
+          this.errorMessage = 'Timeout должен быть больше 5 и меньше 99 секунд'
+          setTimeout(() => {
+            this.errorMessage = ''
+          }, 3000)
+          return
+        }
         const url = window.location.href.includes('localhost') ? 'http://localhost:3002/settings' : '/settings'
         try {
           await axios.post(url, { ...this.params, timeout: this.params.timeout * 1000 })
